50c884cfc7baeb594551c16377318badfb9bdd5a
Eslam Adel <islamadel295@yahoo.com>
2024-08-22 15:48:01 +0300

    disable domain

--- src/Helper/Make/Types/DisableDomain.php
+++ src/Helper/Make/Types/DisableDomain.php
@@ -2,7 +2,9 @@
 
 namespace theaddresstechnology\DDD\Helper\Make\Types;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Traits\Macroable;
 use theaddresstechnology\DDD\Helper\FileCreator;
 use theaddresstechnology\DDD\Helper\Make\Maker;
@@ -72,17 +74,29 @@ class DisableDomain extends Maker
         $service_provider = NamespaceCreator::Segments("Src","Domain",$this->name,"Providers","DomainServiceProvider");
         $app = File::get(base_path('bootstrap'.DIRECTORY_SEPARATOR.'providers.php'));
         //
-       if(Str::of($app)->contains([$service_provider],[false]) ==true) {
-           $content = Str::of($app)->replace($service_provider."::class,","");
+        if(Str::of($app)->contains([$service_provider],[false]) ==true) {
+            $content = Str::of($app)->replace($service_provider."::class,","");
 
-           $this->save(base_path().DIRECTORY_SEPARATOR."bootstrap", 'providers', 'php', $content);
+            $this->save(base_path().DIRECTORY_SEPARATOR."bootstrap", 'providers', 'php', $content);
 
-           $migration_path="src".DIRECTORY_SEPARATOR."Domain".DIRECTORY_SEPARATOR.$this->name.DIRECTORY_SEPARATOR."Database".DIRECTORY_SEPARATOR."Migrations";
+            $migration_path="src".DIRECTORY_SEPARATOR."Domain".DIRECTORY_SEPARATOR.$this->name.DIRECTORY_SEPARATOR."Database".DIRECTORY_SEPARATOR."Migrations";
 
-           \Illuminate\Support\Facades\Artisan::call("migrate:rollback",["--path"=>$migration_path]);
+            $entities=Path::files("src".DIRECTORY_SEPARATOR."Domain".DIRECTORY_SEPARATOR.$this->name.DIRECTORY_SEPARATOR."Entities");
 
-           return true;
-       }
+            foreach ($entities as $entity){
+                $class = NamespaceCreator::entity($this->name,$entity);
+                $table = with(new $class)->getTable();
+                Schema::dropIfExists($table);
+                //dd($table);
+            }
+            $migration_files=Path::files($migration_path);
+            foreach ($migration_files as$migration_file){
+                DB::table('migrations')->where('migration',$migration_file)->delete();
+            }
+            // \Illuminate\Support\Facades\Artisan::call("migrate:rollback",["--path"=>$migration_path,'--force'=>1]);
+
+            return true;
+        }
         error_log("This Domain Is Already Disabled");
 
         return false;
